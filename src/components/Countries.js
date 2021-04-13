55765a9a70c22152b26c3c3495aa84971e328b3b
DUSSARTLuke <lukedussart@hotmail.fr>
2021-04-13 22:10:22 +0200

    premier affichage pays

---
+++ src/components/Countries.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Countries = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag"
+      )
+      .then((res) => setData(res.data));
+  }, []);
+
+  return (
+    <div className="countries">
+      <ul className="countries-list">
+        {data.map((country) => (
+          <li>{country.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Countries;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Logo from "../components/Logo";
 import Navigation from "../components/navigation";
 
 const About = () => {
   return (
     <div>
       <Navigation />
+      <Logo />
       <h1> A propos de </h1>
       <br />
       <p>Lorem ipsum dolor sit amet, consectetur</p>
